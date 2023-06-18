0cbb4c391e7e79b69ebb5899508de088b92eb8dc
isaac-svg <isaacsakyi009@gmail.com>
2023-06-18 17:19:01 +0000

    first commit

--- src/controllers/auth/Register.js
+++ src/controllers/auth/Register.js
@@ -1,19 +1,33 @@
+import { StatusCodes } from "http-status-codes"
 import pool from "../../models/DBconfig.js"
 import { queries } from "../../queries/queries.js"
 import { generateAccountNumber } from "../../util/createAccountNumber.js"
 import bcrypt from "bcryptjs"
+import { getByEmail } from "../../queries/queryFunctions/getByEmail.js"
 
 
 export const Register = async (req,res)=>{
    
     const {first_name,last_name,email,password,address,dob, ghana_card_number,phone} = req.body
 
+    pool.query(queries.checkEmailExists ,[email],(error,result)=>{
+        
+        if(error){
+            console.log(error.message)
+            return {success:"false",message:error.message}
+        }
+       
+       if(result.rowCount){
+     return   res.status(500).json({success:false,
+            message:"This email is already associated with an account"})
+       }
+    }) 
+
     
 
     const salt   =  await bcrypt.genSalt(+process.env.SALT)
     const hashedPassword = await bcrypt.hash(password,salt)
     const account_number =  generateAccountNumber()
-
     pool.query(queries.registerUser,[ first_name , last_name , dob , phone , email , address , ghana_card_number , account_number,hashedPassword],(error,result)=>{
         if(error){
             return res.json(error.message)
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,19 +1,24 @@
 
 
-const getAllStudents = "SELECT * FROM students"
-const getStudentById =  "SELECT * FROM students WHERE id = $1"
-const checkEmailExists = "SELECT s FROM students s WHERE s.email =  $1"
+const getAllStudents = "SELECT * FROM customers"
+const getcustomerByGhanaCard =  "SELECT cus FROM customers WHERE cus.ghana_card_number = $1"
+const getCustomerByPhoneNumber =  "SELECT cus FROM customers WHERE cus.ghana_card_number = $1"
+// const get =  "SELECT * FROM students WHERE email = $1"
+const checkEmailExists = "SELECT * FROM customer  WHERE email =  $1"
 const addStudent= "INSERT INTO students (name,email,dob,age) VALUES ($1, $2, $3, $4) "
 const removeStudent = "DELETE FROM students WHERE id = $1"
 const updateStudent = "UPDATE students SET name = $1 WHERE id = $2"
 
 const registerUser  = "INSERT INTO customer ( first_name , last_name , dob , phone , email , address , ghana_card_number ,account_number,password) VALUES( $1, $2, $3, $4, $5, $6, $7, $8,$9);"
 export const queries={
-    getAllStudents,
-    getStudentById ,
+    // getAllStudents,
+    // getStudentById ,
     checkEmailExists,
-    addStudent,
+    // addStudent,
     removeStudent,
     updateStudent,
-    registerUser
+    registerUser,
+    getCustomerByPhoneNumber,
+    getcustomerByGhanaCard,
+    checkEmailExists
 }
